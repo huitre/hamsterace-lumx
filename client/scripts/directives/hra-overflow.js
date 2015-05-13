fb6db3a944435ff3f1f883ad9a2bd605e177f16a
Huitre <gohin.j@gmail.com>
2015-05-13 19:54:49 +0200

    directive for tabs allowing them to scroll content

---
+++ client/scripts/directives/hra-overflow.js
@@ -0,0 +1,29 @@
+'use strict';
+/*
+exports.inject = function(app) {
+  app.directive('exampleDirective', exports.directive);
+  return exports.directive;
+};
+
+exports.directive = function() {
+  return {
+    restrict: 'E',
+    template: '<ul><li><em>A simple list</em></li><li><em>But I can be anything you want.</em></li></ul>'
+  };
+};
+*/
+
+angular.module('Hamsterace').directive('hraOverflow', ['$document', 
+  function($document) {
+    function link (scope, element, attrs) {
+      element.css({
+       overflowY: 'auto',
+       maxHeight: window.innerHeight - element.offset().top,
+       display: 'block'
+      })
+    }
+    return {
+      link: link
+    }
+  }
+])
