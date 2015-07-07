8ffe8b543b6003c2ff5ef73c8db01b4272628e5e
Huitre <gohin.j@gmail.com>
2015-07-07 22:44:36 +0200

    Ajout directive pour video youtube/vimeo/mp4/ogg

--- client/scripts/directives/embedvideo.js
+++ client/scripts/directives/embedvideo.js
@@ -1,10 +1,28 @@
 angular.module('Hamsterace.Directives').directive(
-'embedVideo', [
-function (element, attrs) {
-    
-    return {
-        link: function (scope, element, attrs) {
-            element.innerHTML = '<video src="' + element.innerText + '"/>';
+'embedVideo', 
+[ '$timeout',
+function ($timeout) {
+  return {
+    restrict : 'E',
+    replace : true,
+    link : function (scope, element, attrs) {
+      $timeout(function () {
+        var videoType = function (src) {
+          var type = [
+                /.*youtube*/,
+                /.*vimeo*/
+              ], code = [
+                '<iframe width="1280" height="750" src="https://www.youtube.com/embed/gDhbpZmhMC4" frameborder="0" allowfullscreen></iframe>',
+                '<iframe src="{{src}}" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+              ]
+          for (var i in type) {
+            if (src.match(type[i]))
+              return code[i]
+          }
+          return '<video src="' + src + '" style="width: 100%" controls></video>';
         }
+        element[0].innerHTML = videoType(element[0].innerText)
+      })
     }
+  }
 }]);
